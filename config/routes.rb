08c1f31ed6ea2a1f1c23815e9d48e2b189c40f0e
otinu <fitshter@gmail.com>
2021-06-02 08:50:34 +0900

    add_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,14 @@
 Rails.application.routes.draw do
-  get 'chats/show'
-  get 'chats/create'
-  get 'chats/destroy'
-  get 'goods/create'
-  get 'goods/destroy'
-  get 'posts/show'
-  get 'posts/index'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'homes/top'
-  get 'homes/about'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  devise_for :users, controllers: { registrations: "users/registrations" }
+  resources :users,only: [:show,:edit,:update]
+
+  resources :posts do
+  resource :goods, only: [:create, :destroy]
+  end
+
+  get 'chat/:id' => 'chats#show', as: 'chat' #URLを単数形の「chat」にしたいため、記述。
+  resources :chats, only: [:create]
+
 end
