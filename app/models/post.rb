e08ba73a344a508798821f13c3063a46ae00da1f
otinu <fitshter@gmail.com>
2021-06-03 16:37:26 +0900

    [add]validation_upload_file

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
-    has_many :goods, dependent: :destroy
+  has_many :goods, dependent: :destroy
+  has_one_attached :post_file #ActiveStrage
 
   #validates :title, presence: true, length: { maximum: 20, minimum: 2 }, uniqueness: true
   #validates :explanation, presence: true, length:  { maximum: 1000 }
-
-  has_one_attached :post_file #ActiveStrage
-
+  validates :post_file, content_type: { in: %w[image/jpeg image/gif image/png application/pdf video/mp4], message: "Please update other file" },
+                        size: { less_than: 3.megabytes, message: "should be less than 3MB" }
+                        
   def favorited_by?(user)
    favorites.where(user_id: user.id).exists?
   end
