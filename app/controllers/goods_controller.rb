d35090180699a9c521cc6162b0e7fa8116d4176c
otinu <fitshter@gmail.com>
2021-06-04 19:29:18 +0900

    [add]goods

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,16 +1,9 @@
 class GoodsController < ApplicationController
   def create
 @post = Post.find(params[:post_id])  #create.js.erbで利用するための準備
-
-=begin
-current_userの持っているpost_idをfavoritesのpost_idへ
-current_userを使いたいから、favoriteモデルはUserモデルともリレーションをもつ。
-
-下記の形で要件定義通りに機能できるのは、postコントローラのshowアクションでjoinsを使ってUserモデルと紐づいているため。
-模範解答はローカル変数に@post.favorites.new(user_id: current_user.id)を格納している。
-自分の作ったものはUserテーブルに新しいレコードを作成しているのに対し、模範解答はpostテーブルに新しいレコードを格納している。
-=end
 good = current_user.goods.new(post_id: params[:post_id])
+good.mine_open = true
+good.others_open = true
 good.save
 end
 
@@ -19,4 +12,13 @@ def destroy
 favorite = current_user.goods.find_by(post_id: params[:post_id])
 favorite.destroy
 end
+
+def count_view
+
+
+
+redirect_to posts_path
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :users,only: [:show,:edit,:update]
 
   resources :posts do
-  resource :goods, only: [:create, :destroy]
+  resource :goods, only: [:create, :destroy,]
   end
+  post 'goods' => 'goods#count_view', as: 'goods_count_view'
 
   get 'chat/:id' => 'chats#show', as: 'chat' #URLを単数形の「chat」にしたいため、記述。
   delete 'chat/:id' => 'chats#destroy', as: 'chat_destroy'
