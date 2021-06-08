b597a3e997bf375d16574625c939d3bc2fe3540e
otinu <fitshter@gmail.com>
2021-06-09 08:17:33 +0900

    add_search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,51 +10,55 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
-    if params[:tag_name]
+    if params[:tag_name] #パラメータでtag_nameが渡ってきた場合はPost.allを上書き
       @posts = Post.tagged_with("#{params[:tag_name]}")
     end
   end
 
-
   def create
-  post = Post.new(post_params)
-  post.user_id = current_user.id
-  if post.save
-  flash[:notice] = "successfully!"
-  redirect_to user_path(current_user)
-  else
-  flash[:alert] = "error / can't be blank or over 200words"
-  flash[:notice] = "can't be blank"
-  redirect_to posts_path
-  end
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    if post.save
+      flash[:notice] = "successfully!"
+      redirect_to user_path(current_user)
+    else
+      flash[:alert] = "error / can't be blank or over 200words"
+      flash[:notice] = "can't be blank"
+      redirect_to posts_path
+    end
   end
 
   def edit
-  @post = Post.find(params[:id])
-  unless current_user.id == @post.user_id
-  redirect_to posts_path
-  end
+    @post = Post.find(params[:id])
+    unless current_user.id == @post.user_id
+      redirect_to posts_path
+    end
   end
 
   def update
-  @post = Post.find(params[:id])
-  if @post.update(post_params)
-  redirect_to post_path(@post), notice: "You have updated book successfully."
-  else
-  render "edit", notice: "error / can't be blank or over 200words"
-  end
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "You have updated book successfully."
+    else
+      render "edit", notice: "error / can't be blank or over 200words"
+    end
   end
 
   def destroy
-  @post = Post.find(params[:id])
-  @post.destroy
-  redirect_to posts_path
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def search
+    @posts = Post.search_for(params[:keyword]) #クラスメソッド使用
+    render "index"
   end
 
   private
 
   def post_params
-  params.require(:post).permit(:title, :explanation, :user_id, :post_file, :tag_list)
+    params.require(:post).permit(:title, :explanation, :user_id, :post_file, :tag_list)
   end
 
 end
