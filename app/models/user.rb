15f8da1b92ed3bb2085261324c8672167dd073ad
otinu <fitshter@gmail.com>
2021-06-13 19:23:50 +0900

    [fix]layout_post/index_post/show

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,16 +16,21 @@ class User < ApplicationRecord
 
   has_many :active_notifications, class_name: "Notification", foreign_key: "visiter_id", dependent: :destroy
   has_many :passive_notifications, class_name: "Notification", foreign_key: "visited_id", dependent: :destroy
+  
+  #==================================================== バリデーション =================================================== 
 
   validates :name, length: { maximum: 20, minimum: 2 }, uniqueness: true
-  # validates :introduction, length: { maximum: 50 }
+  validates :introduction, length: { maximum: 50 }
   validates :profile_image, content_type: { in: %w(image/jpeg image/gif image/png) }, size: { less_than: 7000.kilobytes, message: "should be less than 7000KB" }
+  
+    #====================================================   メソッド   =================================================== 
 
   # プロフィール画像の大きさを調整
   def user_image_resize
     profile_image.variant(gravity: :center, resize: "200x200^", crop: "210x210+0+0").processed
   end
 
+  # SNS認証
   def self.find_omniauth(auth)
     begin # 例外発生時はuserにnilを格納し、コントローラでのrescue処理へ誘導
       sns_credentials = SnsCredential.find_by(uid: auth.uid, provider: auth.provider)
