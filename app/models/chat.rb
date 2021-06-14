598dcb597382ca71ba4b8237c70d9634268a3bd9
otinu <fitshter@gmail.com>
2021-06-15 07:09:01 +0900

    [fix]layout_chat_notification

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,6 @@
 class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validates :message, length: { maximum: 50, minimum: 1 }
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   has_many :chats
   has_many :user_rooms
+  
+  validates :name, length: { maximum: 20, minimum: 2 }, uniqueness: true
 end
