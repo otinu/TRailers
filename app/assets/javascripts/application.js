892a1511b35d67a908f6430375f69f49019c27e3
otinu <fitshter@gmail.com>
2021-06-25 11:16:25 +0900

    [add]mine/others_open_JS_comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,11 +21,35 @@
 //= require_tree .
 
 
+/*================= 投稿一覧 ================= */
+
 $(document).on('turbolinks:load',function(){
   $('#arrow_top').on('click',function(event){
     $('body, html').animate({
       scrollTop:0
     }, 800);
     event.isDefaultPrevented();
+  });
+});
+
+/*================= ユーザー詳細ページ ================= */
+
+$(document).on('turbolinks:load',function(){
+  $('.mine-comment').hide();
+  $('.mine-open').mouseover(function(){
+    $('.mine-comment').css({'display': ''});
+  });
+  $('.mine-open').mouseout(function(){
+    $('.mine-comment').css({'display': 'none'});
+  });
+});
+
+$(document).on('turbolinks:load',function(){
+  $('.others-comment').hide();
+  $('.others-open').mouseover(function(){
+    $('.others-comment').css({'display': ''});
+  });
+  $('.others-open').mouseout(function(){
+    $('.others-comment').css({'display': 'none'});
   });
 });
