fbe919af709f3c28a62d0c89311e494237f02901
otinu <fitshter@gmail.com>
2021-06-26 21:36:14 +0900

    [fix]readable

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,14 +2,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     super
     if (params[:user][:email]).present?
-      ThanksMailer.send_signup_email(params[:user][:email], params[:user][:name]).deliver if (params[:user][:email]).blank?
+      if resource.valid?
+        ThanksMailer.send_signup_email(params[:user][:email], params[:user][:name]).deliver
+      end
     end
-=begin
-    begin
-      ThanksMailer.send_signup_email(params[:user][:email], params[:user][:name]).deliver if (params[:user][:email]).blank?
-    rescue
-      redirect_to new_registration_path(resource_name) && return
-    end
-=end
   end
 end
