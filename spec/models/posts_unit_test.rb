64c90f9daf7fd893476b4354f4dd0a15d122faf9
otinu <fitshter@gmail.com>
2021-07-24 08:06:04 +0900

    [fix]posts_unit_test

--- spec/models/posts_unit_test.rb
+++ spec/models/posts_unit_test.rb
@@ -15,17 +15,20 @@ describe '【Max Red5】Postモデルのテスト', type: :model do
 
   it "【Red1】タイトルが21文字以上の投稿は保存されないか" do
     @post.title = Faker::Lorem.characters(number: 21)
-    expect(@post).to be_valid
+    @post.valid?
+    expect(@post.errors.full_messages).to eq ["投稿名は2～20文字以内で入力してください"]
   end
 
   it "【Red2】タイトルが1文字の投稿は保存されないか" do
     @post.title = Faker::Lorem.characters(number: 1)
-    expect(@post).to be_valid
+    @post.valid?
+    expect(@post.errors.full_messages).to eq ["投稿名は2～20文字以内で入力してください"]
   end
 
   it "【Red3】投稿へのコメントが501文字以上の投稿は保存されないか" do
     @post.explanation = Faker::Lorem.characters(number: 501)
-    expect(@post).to be_valid
+    @post.valid?
+    expect(@post.errors.full_messages).to eq ["投稿へのコメントは500文字以内でお願いします"]
   end
 
   it "動画投稿は保存されるか" do
@@ -40,11 +43,13 @@ describe '【Max Red5】Postモデルのテスト', type: :model do
 
   it "【Red4】容量オーバーの投稿は保存されないか" do
     @post.post_file = fixture_file_upload("/post_over_test.mp4")
-    expect(@post).to be_valid
+    @post.valid?
+    expect(@post.errors.full_messages).to eq ["投稿ファイルは3MBまででお願いします"]
   end
 
   it "【Red5】JPEG,GIF,PNG,PDF,MP4以外の投稿は保存されないか" do
     @post.post_file = fixture_file_upload("/test.css")
-    expect(@post).to be_valid
+    @post.valid?
+    expect(@post.errors.full_messages).to eq ["投稿ファイルのファイル形式では送信できません 他のファイル形式で送信をお願いします"]
   end
 end
