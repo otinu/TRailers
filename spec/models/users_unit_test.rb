f9eb72c059166c7e92c1ff4680e2451b2f497490
otinu <fitshter@gmail.com>
2021-07-23 10:31:55 +0900

    [fix]users_unit_test

--- spec/models/users_unit_test.rb
+++ spec/models/users_unit_test.rb
@@ -4,25 +4,28 @@ describe '【Max Red7】 Userモデルのテスト', type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
-
-  context "プロフィール画像投稿前のテスト" do
+  
     it "有効なユーザー情報の場合は保存されるか" do
       expect(@user).to be_valid
     end
-
-    it "【Red1】名前が21文字以上のユーザー情報は保存されないか" do
+    
+  context "入力のテスト" do
+    it "名前が21文字以上のユーザー情報は保存されないか" do
       @user.name = Faker::Lorem.characters(number: 21)
-      expect(@user).to be_valid
+      @user.valid?
+      expect(@user.errors.full_messages).to eq ["名前は20文字以内で入力してください"]
     end
 
-    it "【Red2】タイトルが1文字のユーザー情報は保存されないか" do
-      @user.title = Faker::Lorem.characters(number: 1)
-      expect(@user).to be_valid
+    it "名前が1文字のユーザー情報は保存されないか" do
+      @user.name = Faker::Lorem.characters(number: 1)
+      @user.valid?
+      expect(@user.errors.full_messages).to eq ["名前は2文字以上で入力してください"]
     end
 
-    it "【Red3】自己紹介文が51文字以上のユーザー情報は保存されないか" do
-      @user.explanation = Faker::Lorem.characters(number: 51)
-      expect(@user).to be_valid
+    it "自己紹介文が51文字以上のユーザー情報は保存されないか" do
+      @user.introduction = Faker::Lorem.characters(number: 51)
+      @user.valid?
+      expect(@user.errors.full_messages).to eq ["自己紹介は50文字以内で入力してください"]
     end
   end
 
@@ -32,24 +35,28 @@ describe '【Max Red7】 Userモデルのテスト', type: :model do
       expect(@user.profile_image).to be_valid
       expect(@user).to be_valid
     end
-    it "【Red4】動画投稿は保存されないか" do
+    it "動画投稿は保存されないか" do
       @user.profile_image = fixture_file_upload("/test.mp4")
-      expect(@user).to be_valid
+      @user.valid?
+      expect(@user.errors.full_messages).to include("プロフィール画像はJPEG、GIF、PNG画像のいずれかでお選びください")
     end
 
-    it "【Red5】PDF投稿は保存されないか" do
+    it "PDF投稿は保存されないか" do
       @user.profile_image = fixture_file_upload("/test.pdf")
-      expect(@user).to be_valid
+      @user.valid?
+      expect(@user.errors.full_messages).to include("プロフィール画像はJPEG、GIF、PNG画像のいずれかでお選びください")
+    end
+    
+    it "JPEG,GIF,PNG以外の投稿は保存されないか" do
+      @user.profile_image = fixture_file_upload("/test.css")
+      @user.valid?
+      expect(@user.errors.full_messages).to include("プロフィール画像はJPEG、GIF、PNG画像のいずれかでお選びください")
     end
 
-    it "【Red6】容量オーバーの投稿は保存されないか" do
-      @post.post_file = fixture_file_upload("/profile_over_test.mp4")
-      expect(@post).to be_valid
-    end
-
-    it "【Red7】JPEG,GIF,PNG以外の投稿は保存されないか" do
-      @user.post_file = fixture_file_upload("/test.css")
-      expect(@user).to be_valid
+    it "容量オーバーの投稿は保存されないか" do
+      @user.profile_image = fixture_file_upload("/profile_over_test.png")
+      @user.valid?
+      expect(@user.errors.full_messages).to include("プロフィール画像は100KBまででお願いします")
     end
   end
 end
