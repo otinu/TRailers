860d79e614d382f5e5d435bf7352382906b5fb7a
otinu <fitshter@gmail.com>
2021-07-25 10:37:16 +0900

    add_test/goods_plus_show

--- spec/system/goods_integration_test.rb
+++ spec/system/goods_integration_test.rb
@@ -10,8 +10,17 @@ let!(:post) { FactoryBot.build(:post) }
     expect(page).to have_content "【ジャンル】Ruby"
   end
   
-  it "Goodsボタンの動作確認" do
+  it "投稿一覧ページでのGoodsボタン動作確認" do
     within "div#1" do
+      find('.fa-hand-holding-heart').click
+      expect("div#1").to have_text '1' 
+    end
+  end
+  it "投稿詳細ページでのGoodsボタン動作確認" do
+    within "div#1" do
+        find('.post-glass').click
+    end
+     within "div#1" do
       find('.fa-hand-holding-heart').click
       expect("div#1").to have_text '1' 
     end
