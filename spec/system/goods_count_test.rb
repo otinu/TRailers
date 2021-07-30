5371b5aa1cda5aa4c65ce2489c285fb7720374ba
otinu <fitshter@gmail.com>
2021-07-31 06:50:12 +0900

    [fix]goods_count_test/text

--- spec/system/goods_count_test.rb
+++ spec/system/goods_count_test.rb
@@ -6,28 +6,28 @@ let!(:user2) { FactoryBot.build(:user, :second) }
 let!(:post) { FactoryBot.build(:post) }
 let(:rspec_session) { { user_id: 1 } }
 
-  before "2ユーザー間共有の投稿データを生成" do
+  before "一人目のユーザーのログインからテストを進める" do
     sign_up_as(user1)  # SignupSupportモジュールから呼び出し
   end
-    it "自分のGoods数非公開" do
-      new_post(post)
-      visit user_path(user1)
-      find('.fa-display-slash').click
-      visit posts_path
-      expect("div#1").to have_text '1'
-      click_on 'Logo'
-      sign_up_same_session(user2)
-      visit posts_path
-      has_no_field?("div#1")
-    end
-    it "他人のGoods数を非表示" do
-      new_post(post)
-      expect("div#1").to have_text '1'
-      click_on 'Logo'
-      sign_up_same_session(user2)
-      visit user_path(user2)
-      find('.fa-eye-slash').click
-      visit posts_path
-      has_no_field?("div#1")
-    end
+  it "自分のGoods数非公開" do
+    new_post(post)
+    visit user_path(user1)
+    find('.fa-display-slash').click
+    visit posts_path
+    expect("div#1").to have_text '1'
+    click_on 'Logo'
+    sign_up_same_session(user2)
+    visit posts_path
+    has_no_field?("div#1")
+  end
+  it "他人のGoods数を非表示" do
+    new_post(post)
+    expect("div#1").to have_text '1'
+    click_on 'Logo'
+    sign_up_same_session(user2)
+    visit user_path(user2)
+    find('.fa-eye-slash').click
+    visit posts_path
+    has_no_field?("div#1")
+  end
 end
