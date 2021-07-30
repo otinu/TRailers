016e8182c2c993f6e19ca6f73b9dc36ffa794d64
otinu <fitshter@gmail.com>
2021-07-31 04:11:59 +0900

    [reset]development_S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   config.action_mailer.raise_delivery_errors = true
 
