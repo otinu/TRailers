1d928962c18d56c316ceb2406168cb9c63e7315b
otinu <fitshter@gmail.com>
2021-07-31 06:37:23 +0900

    [add]mine_open_test

--- spec/support/signup_support.rb
+++ spec/support/signup_support.rb
@@ -7,6 +7,10 @@ module SignupSupport
     fill_in 'user[password_confirmation]', with: user.password
     check 'user[mine_open]'
     click_button '新規登録'
-
+  end
+  
+  def sign_up_same_session(user)
+    allow_any_instance_of(ActionDispatch::Request).to receive(:session){ { id: 1 } }
+    sign_up_as(user2)
   end
 end
--- spec/system/goods_count_test.rb
+++ spec/system/goods_count_test.rb
@@ -4,24 +4,25 @@ describe 'Goods数の公開・表示テスト' do
 let!(:user1) { FactoryBot.build(:user, :first) }
 let!(:user2) { FactoryBot.build(:user, :second) }
 let!(:post) { FactoryBot.build(:post) }
+let(:rspec_session) { { user_id: 1 } }
+
+  before "2ユーザー間共有の投稿データを生成" do
+    post = Post.create(user_id: 1, title: "Get set", tag_list: "Rails", explanation: "テスト用の投稿です")
+    post.post_file.attach(io: File.open('app/assets/images/HTML_sample.pdf'), filename: 'HTML_sample.pdf')
+    sign_up_as(user1)  # SignupSupportモジュールから呼び出し
+  end
 
   context "自分のGoods数非公開" do
     it "自分のGoods数を非公開に変更" do
-        sign_up_as(user1)  # SignupSupportモジュールから呼び出し
-        new_post(post)
-        expect(page).to have_content "【ジャンル】Ruby"
-        visit user_path(user1)
-        find('.fa-display-slash').click
-        visit posts_path
-        within "div#1" do
-          expect("div#1").to have_text '1'
-        end
-        click_on 'Logo'
-      end
-    it "別ユーザーで非公開を確認" do
-      sign_up_as(user2)
+      new_post(post)
+      visit user_path(user1)
+      find('.fa-display-slash').click
       visit posts_path
-      expect(page).to have_no_css "div#1"
+      expect("div#1").to have_text '1'
+      click_on 'Logo'
+      sign_up_same_session(user2)
+      visit posts_path
+      has_no_field?("div#1")
     end
   end
 end
