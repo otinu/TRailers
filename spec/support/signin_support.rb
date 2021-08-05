326ad68dd3493ab8a2cba29064c21e45817b95df
otinu <fitshter@gmail.com>
2021-08-05 09:46:21 +0900

    [add]OAuth_integration_test_Twitter

--- spec/support/signin_support.rb
+++ spec/support/signin_support.rb
@@ -13,4 +13,18 @@ module SigninSupport
       }
     )
   end
+  def twitter_oauth_mock
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new(
+      provider: 'twitter',
+      uid: rand(100000000..999999999),
+      info: {
+        :name => Faker::Lorem.characters(number: 10),
+        :email => Faker::Internet.email
+      },
+      credentials: {
+        token: 'token'
+      }
+    )
+  end
 end
--- spec/system/OAuth_integration_test.rb
+++ spec/system/OAuth_integration_test.rb
@@ -15,4 +15,18 @@ describe "SNS認証のテスト" do
       expect(current_path).to match (/posts/)
     end
   end
+  
+  context "Twitterでログイン" do
+    let!(:user) { FactoryBot.build(:user, :third) }
+    before do
+      OmniAuth.config.mock_auth[:twitter] = nil
+      Rails.application.env_config["devise.mapping"] = Devise.mappings[:user] 
+      Rails.application.env_config['omniauth.auth'] = twitter_oauth_mock()
+    end
+    it "responds successfully" do
+      visit new_user_session_path
+      find(".fa-twitter-square").click
+      expect(current_path).to match (/posts/)
+    end
+  end
 end
